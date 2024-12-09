68d8be59ce5256a8f1df269282d8d192cf24af5e
Drago <ivan.drago.ussr.true@gmail.com>
2024-12-10 00:50:34 +0100

    1

--- src/database/database.go
+++ src/database/database.go
@@ -1,11 +1,13 @@
 package database
 
 import (
+	"os"
+	"strconv"
+	"time"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
-	"os"
-	"strconv"
 )
 
 type Service struct {
@@ -18,6 +20,19 @@ func NewService(mysqlURL string) (*Service, error) {
 	if err != nil {
 		panic("failed to connect database")
 	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		panic("failed to open database:" + err.Error())
+	}
+	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
+	sqlDB.SetMaxIdleConns(10)
+
+	// SetMaxOpenConns sets the maximum number of open connections to the database.
+	sqlDB.SetMaxOpenConns(20)
+
+	// SetConnMaxLifetime sets the maximum amount of time a connection may be reused.
+	sqlDB.SetConnMaxLifetime(time.Hour)
 
 	debugMode := os.Getenv("MYSQL_DEBUG_MODE")
 	if debugMode != "" {
